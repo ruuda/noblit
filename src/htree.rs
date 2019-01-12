5572a825e61a1d2201c5f6a203a9b80df171fea7
Ruud van Asseldonk <dev@veniogames.com>
2019-01-12 12:03:03 +0100

    Begin rewriting tree iterator again
    Now it is just an iterator for an array. Next thing is to also iterate
    child nodes, and assuming that there are only pending nodes in the
    leaves. And then that restriction must be lifted, to merge the pending
    datoms with the child iteration ones.

--- src/htree.rs
+++ src/htree.rs
@@ -150,21 +150,12 @@ pub fn write_tree<S: Store>(store: &mut S, datoms: &[Datom]) -> io::Result<PageI
 }
 
 /// Indicates how to reach a datom in the tree.
-#[derive(Eq, PartialEq)]
 struct Route {
-    /// The pages to traverse from the root.
+    /// Index into the datom array in the node, of the next pending datom to yield.
     ///
-    /// The first element is the root node, the last element is the node that
-    /// contains the datom.
-    // TODO: Could use a small slice: depth will not be more than 9 anyway for a
-    // branching factor of 102: more datoms are not addressable with 64 bits.
-    pages: Vec<PageId>,
-
-    /// Index of the datom in the datoms array that we followed.
-    ///
-    /// For the last element, this is the index into the `datoms` array. For the
-    /// preceding elements, this is the index into the `children` array.
-    indices: Vec<usize>,
+    /// When the index points past the datom array, or when the datom at the
+    /// given index is a midpoint, then the pending datoms have been exhausted.
+    pending_index: usize,
 }
 
 /// A hittchhiker tree.
@@ -188,64 +179,22 @@ impl<'a, Cmp: DatomOrd, S: Store> HTree<'a, Cmp, S> {
         Node::from_bytes::<S::Size>(self.store.get(page))
     }
 
-    /// Locate the last datom that is smaller than or equal to the queried one.
+    /// Locate the first datom that is greater than or equal to the queried one.
     pub fn find(&self, datom: &Datom) -> Route {
-        let mut pages = vec![self.root_page];
-        let mut indices = vec![0];
+        let node = self.get(self.root_page);
 
-        loop {
-            let mut node = self.get(*pages.last().unwrap());
-
-            // Do a linear scan over the datoms in the current node and stop
-            // at the first one that is greater than or equal to the requested
-            // datom. TODO: In the future we might do a binary search.
-            for (i, midpoint) in node.datoms.iter().enumerate() {
-                match self.comparator.cmp(datom, midpoint) {
-                    Ordering::Less => continue,
-                    Ordering::Equal => {
-                        // We found the datom. Assuming datoms are unique, we
-                        // can return the route to it.
-                        *indices.last_mut().unwrap() = i;
-                        return Route {
-                            pages: pages,
-                            indices: indices,
-                        };
-                    }
-                    Ordering::Greater => {
-                        // We stepped too far. The target datom is either at the
-                        // previous index, or it is in a child node, if there is
-                        // any.
-                        // TODO: No, this is incorrect, the pending datoms are
-                        // not midpoints. We need to track two bounds.
-                        let child_index = node.children[i];
-                        if child_index == PageId::max() {
-                            // TODO: What if i = 0? Then the cursor points to
-                            // before the first element.
-                            *indices.last_mut().unwrap() = i - 1;
-                            return Route {
-                                pages: pages,
-                                indices: indices,
-                            };
-                        }
-
-                        // There is a child node, descend into it.
-                        node = self.get(child_index);
-                        pages.push(child_index);
-                        indices.push(0);
-                        continue;
-                    }
-                }
+        for (i, datom_i) in node.datoms.iter().enumerate() {
+            match self.comparator.cmp(datom_i, datom) {
+                Ordering::Less => continue,
+                Ordering::Equal | Ordering::Greater => return Route {
+                    pending_index: i,
+                },
             }
+        }
 
-            // We exhausted the current node. All datoms stored in it are
-            // smaller than the requested datom. If there were parent nodes,
-            // then the datoms in it were larger than those in this node, so
-            // we found the candidate.
-            *indices.last_mut().unwrap() = node.datoms.len();
-            return Route {
-                pages: pages,
-                indices: indices,
-            };
+        // Everything is less than the given datom, return a route past the end.
+        Route {
+            pending_index: node.datoms.len(),
         }
     }
 }
@@ -268,7 +217,7 @@ impl<'a, Cmp: DatomOrd, S: Store> Iter<'a, Cmp, S> {
     fn new(tree: &'a HTree<'a, Cmp, S>, begin: Route, end: Route) -> Iter<'a, Cmp, S> {
         Iter {
             tree: tree,
-            node: tree.get(*begin.pages.last().unwrap()),
+            node: tree.get(tree.root_page),
             begin: begin,
             end: end,
         }
@@ -279,46 +228,17 @@ impl<'a, Cmp: DatomOrd, S: Store> Iterator for Iter<'a, Cmp, S> {
     type Item = &'a Datom;
 
     fn next(&mut self) -> Option<&'a Datom> {
-        let mut index = *self.begin.indices.last().unwrap();
-
-        // First of all, try to move into a child node, if there is any.
-        let child_page = self.node.children[index];
-        if child_page != PageId::max() {
-            self.begin.pages.push(child_page);
-            self.begin.indices.push(0);
-            self.node = self.tree.get(child_page);
-
-            // Tail call: yield from the child page.
-            return self.next();
+        // Check if we exhausted the range.
+        if self.begin.pending_index == self.end.pending_index {
+            return None
         }
 
-        // TODO: Check if we reached the end, and stop if so.
+        // We did not exhaust the range, locate the next datom to yield.
+        let datom = &self.node.datoms[self.begin.pending_index];
 
-        loop {
-            // There are no children left at the curent index, so we can yield
-            // the midpoint datom, if there is any. If we exhausted all datoms
-            // in this node, then we need to move up one node in the tree.
-            if index < self.node.datoms.len() - 1 {
-                // There is a midpoint datom, yield it.
-                let result = &self.node.datoms[index];
-                *self.begin.indices.last_mut().unwrap() += 1;
-                return Some(result);
-            } else {
-                // We exhausted this node, move up one node, then loop. If we
-                // loop, we will yield the datom that is there, otherwise we
-                // will move up all the way if the tree is exhausted.
-                self.begin.pages.pop();
-                self.begin.indices.pop();
-                if let Some(parent_page) = self.begin.pages.last() {
-                    // There is still a parent. Move up.
-                    index = *self.begin.indices.last().unwrap();
-                    self.node = self.tree.get(*parent_page);
-                } else {
-                    // The tree is exhausted.
-                    return None
-                }
-            }
-        }
+        self.begin.pending_index += 1;
+
+        Some(datom)
     }
 }
 
@@ -360,7 +280,7 @@ mod test {
             .map(|i| Datom::assert(Eid(i), Aid::max(), Value::min(), Tid::max()))
             .collect();
         let child_ids: Vec<_> = iter::repeat(PageId::max())
-            .take(13)
+            .take(datoms.len())
             .collect();
 
         let node = Node {
@@ -384,12 +304,10 @@ mod test {
             tree: &tree,
             node: tree.get(tree.root_page).clone(),
             begin: Route {
-                pages: vec![page],
-                indices: vec![0],
+                pending_index: 0,
             },
             end: Route {
-                pages: vec![page],
-                indices: vec![0],
+                pending_index: datoms.len(),
             },
         };
 
