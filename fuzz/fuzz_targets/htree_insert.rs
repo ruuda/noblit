909ee345376ce805f73416cd53df03a6b3acf805
Ruud van Asseldonk <dev@veniogames.com>
2019-02-20 20:12:05 +0100

    Use slices of different length in fuzzer
    Right now, the generated slices do not satisfy the precondition that the
    datoms be sorted, so it is expected that counterexamples may be found.
    The fuzzer output is not very though, there is only the backtrace, and I
    can't even see the panic message.
    
    I should make a generic fuzzing harness, including quickcheck-like
    generators that I am now writing in an ad-hoc way. Then I could write
    fuzz tests in there, and hook them up to the fuzzer for running, but
    once fuzzing finds a counterexample, I want to be able to feed that into
    a different binary that then prints the interpretation of the seed in a
    human-readable form, followed by violating the assertion, so I can see
    the message.

--- fuzz/fuzz_targets/htree_insert.rs
+++ fuzz/fuzz_targets/htree_insert.rs
@@ -4,26 +4,48 @@
 extern crate libfuzzer_sys;
 extern crate noblit;
 
+use std::cmp;
+
 use noblit::datom::{Datom, Aid, Eid, Value, Tid};
 use noblit::htree::{HTree, Node};
 use noblit::store::{MemoryStore, PageSize, Store};
 
-fn run<Size: PageSize>(data: &[u8]) {
-    let make_datom = |i, t| Datom::assert(Eid(i), Aid::max(), Value::min(), Tid(t));
+/// Evaluate a closure on byte slices of various lengths.
+fn for_slices<F>(data: &[u8], mut f: F) where F: FnMut(&[u8]) {
+    let mut left = data;
 
+    while left.len() > 2 {
+        // Read a 16-bit length prefix.
+        let len = cmp::min(
+            (left[0] as usize) << 8 | (left[1] as usize),
+            left.len() - 2,
+        );
+
+        f(&left[2..2 + len]);
+        left = &left[2 + len..];
+    }
+}
+
+fn run<Size: PageSize>(full_data: &[u8]) {
     let mut store = MemoryStore::<Size>::new();
     let node = Node::empty_of_level(0);
     let root = store.write_page(&node.write::<Size>()).unwrap();
     let comparator = ();
     let mut tree = HTree::new(root, &comparator, store);
 
+    let mut tid = 0;
+
     // Insert one datom at a time, with increasing transaction id in order not
     // to create duplicates.
-    for (t, &i) in data.iter().enumerate() {
-        let tid = 2 * t as u64; // Transaction id must be even.
-        let datoms = vec![make_datom(i as u64, tid)];
+    for_slices(full_data, |xs| {
+        tid += 2; // Transaction id must be even.
+        // TODO: Skip if there are duplicates, or if non-increasing.
+        let datoms: Vec<Datom> = xs.iter().map(|&x|
+            Datom::assert(Eid(x as u64), Aid::max(), Value::min(), Tid(tid))
+        ).collect();
+
         tree.insert(&datoms[..]).unwrap();
-    }
+    });
 }
 
 fuzz_target!(|data: &[u8]| {
