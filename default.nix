308ba181bf9e8e6b4961a5ad25af1f00be926a0f
Ruud van Asseldonk <dev@veniogames.com>
2019-10-01 20:06:02 +0200

    Add Nix-based build environment, use it on CI
    I am tired of having to fix missing packages, or having the wrong
    versions of packages, on CI all the time. It is fixable, but it turns
    into a very specific configuration that works for Travis' interpretation
    of Ubuntu, and it is getting out of hand bit by bit.
    
    A separate problem is reproducibility: if I check out a commit two years
    from now, I still want to be able to build and run Noblit. This implies
    pinning everything -- dependencies, but also the entire toolchain. Parts
    are already pinned: Rustup pins Cargo and Rustc, Stack pins GHC and
    Haskell dependencies. Now I add Nix to pin Rustup and Stack, and also
    some other development dependencies such as Python that are used to run
    the tests.
    
    So Nix solves the reproducibility problem. It also isolates
    dependencies, which means I can use the same build environment here, and
    on CI. That should make CI a lot easier to debug. And it also solves the
    toolchain problem neatly: if the Nix environment works locally, then it
    will work on CI too.
    
    An additional advantage is that this reduces vendor lock-in on the CI
    provider. Anything that can run Nix can now create an environment with
    the right tools to build Noblit.

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import (import ./nixpkgs-pinned.nix) {};
+in
+  pkgs.buildEnv {
+    name = "noblit-devenv";
+    paths = [
+      pkgs.mkdocs  # For building documentation.
+      pkgs.mypy    # For typechecking the Python code.
+      pkgs.perl    # The "prove" test harness is part of Perl.
+      pkgs.python3 # For running the golden tests.
+      pkgs.rustup  # Provides the Rust toolchain.
+      pkgs.stack   # Provides the Haskell toolchain.
+    ];
+  }
---
+++ nixpkgs-pinned.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/d484f2b7fc0834a068e8ace851faa449a03963f5.tar.gz";
+  sha256 = "0jk93ikryi2hqc30l2n5i4vlgmklrlzb8cf7b3sg1q3k70q344jn";
+}
