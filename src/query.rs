fe4aa3849c4def7b331b2001af39b68de9eedbf6
Ruud van Asseldonk <dev@veniogames.com>
2019-10-05 22:49:42 +0200

    Define parser for binary transaction requests
    Finding a good name for this thing is hard. It is not a transaction; the
    transaction is what gets created as a result. It is not a query either,
    because queries only read things, but this is a write. It does include a
    query as a subset though.
    
    "Assertion" is close, but the thing does include a query part, so
    assertion in itself is not the full story.
    
    Perhaps the difficulty to name this thing is a sign that it's the wrong
    thing. Perhaps I should not merge queries into writes after all, and let
    the client library deal with it. Then a transaction request would really
    just be a list of statements.
    
    But for now, let's see where this goes.

--- src/query.rs
+++ src/query.rs
@@ -29,21 +29,21 @@ pub struct Var(pub u16);
 /// Named attributes are converted into fixed attribute ids at the start
 /// of evaluation. Names provide a level of indirection: attribute names
 /// may change, but their ids will not.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum QueryAttribute {
     Named(String),
     Fixed(Aid),
 }
 
 /// The value in a query can be a constant or a variable.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum QueryValue {
     Const(Value),
     Var(Var),
 }
 
 /// A statement that relates an entity, attribute, and a value.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Statement {
     pub entity: Var,
     pub attribute: QueryAttribute,
@@ -68,7 +68,7 @@ impl Statement {
     }
 }
 
-/// A query.
+/// A query (read-only).
 pub struct Query {
     /// A human-meaningful name for every variable.
     pub variable_names: Vec<String>,
@@ -81,8 +81,10 @@ pub struct Query {
 }
 
 impl Query {
-    /// Deserialize a query in binary format.
-    pub fn parse(cursor: &mut Cursor) -> Result<Query, CursorError> {
+    // TODO: Move binary format parsing into its own module.
+
+    /// Deserialize variable names from binary format.
+    fn parse_strings(cursor: &mut Cursor) -> Result<Vec<String>, CursorError> {
         let num_variables = cursor.take_u16_le()?;
         let mut variable_names = Vec::with_capacity(num_variables as usize);
         for _ in 0..num_variables {
@@ -91,9 +93,14 @@ impl Query {
             variable_names.push(name.to_string());
         }
 
-        let num_where_statements = cursor.take_u16_le()?;
-        let mut where_statements = Vec::with_capacity(num_where_statements as usize);
-        for _ in 0..num_where_statements {
+        Ok(variable_names)
+    }
+
+    /// Deserialize statements from binary format.
+    fn parse_statements(cursor: &mut Cursor) -> Result<Vec<Statement>, CursorError> {
+        let num_statements = cursor.take_u16_le()?;
+        let mut statements = Vec::with_capacity(num_statements as usize);
+        for _ in 0..num_statements {
             let entity_var = cursor.take_u16_le()?;
             let attribute_len = cursor.take_u16_le()?;
             let attribute = cursor.take_utf8(attribute_len as usize)?;
@@ -122,15 +129,28 @@ impl Query {
                 attribute: QueryAttribute::Named(attribute.to_string()),
                 value: value,
             };
-            where_statements.push(statement);
+            statements.push(statement);
         }
 
-        let num_selects = cursor.take_u16_le()?;
-        let mut selects = Vec::with_capacity(num_selects as usize);
-        for _ in 0..num_selects {
+        Ok(statements)
+    }
+
+    fn parse_variables(cursor: &mut Cursor) -> Result<Vec<Var>, CursorError> {
+        let num_variables = cursor.take_u16_le()?;
+        let mut variables = Vec::with_capacity(num_variables as usize);
+        for _ in 0..num_variables {
             let var = cursor.take_u16_le()?;
-            selects.push(Var(var));
+            variables.push(Var(var));
         }
+
+        Ok(variables)
+    }
+
+    /// Deserialize a query in binary format.
+    pub fn parse(cursor: &mut Cursor) -> Result<Query, CursorError> {
+        let variable_names = Query::parse_strings(cursor)?;
+        let where_statements = Query::parse_statements(cursor)?;
+        let selects = Query::parse_variables(cursor)?;
 
         let query = Query {
             variable_names: variable_names,
@@ -164,7 +184,7 @@ impl Query {
         }
     }
 
-    /// Infer the type of every variables.
+    /// Infer the type of every variable.
     pub fn infer_types<
         Store: store::Store,
         Pool: pool::Pool,
@@ -219,5 +239,96 @@ impl Query {
             .collect();
 
         Ok(result)
+    }
+}
+
+/// A request to transact new datoms.
+pub struct QueryMut {
+    /// A human-meaningful name for every variable.
+    pub variable_names: Vec<String>,
+
+    /// Relations that bind variables, in order to refer to existing entities.
+    pub where_statements: Vec<Statement>,
+
+    /// New datoms to insert in this transaction.
+    pub assertions: Vec<Statement>,
+
+    /// Variables bound by the "where" part of the query.
+    pub bound_variables: Vec<Var>,
+
+    /// Free variables, for which we need to create new entities.
+    pub free_variables: Vec<Var>,
+
+    /// The variables to return results for, and their order.
+    pub select: Vec<Var>,
+}
+
+impl QueryMut {
+    /// Deserialize a transaction request in binary format.
+    pub fn parse(cursor: &mut Cursor) -> Result<QueryMut, CursorError> {
+        use std::iter;
+
+        // The first part is the same as for a regular query.
+        let variable_names = Query::parse_strings(cursor)?;
+        let where_statements = Query::parse_statements(cursor)?;
+        let selects = Query::parse_variables(cursor)?;
+
+        // Then we get the assertions.
+        let assertions = Query::parse_statements(cursor)?;
+
+        // Determine which variables are bound, by marking all variables that
+        // occur in the "where" part as bound.
+        let mut is_bound: Vec<bool> = iter::repeat(false)
+            .take(variable_names.len())
+            .collect();
+
+        for statement in where_statements.iter() {
+            // TODO: Guard against malicious inputs, report error on index out
+            // of bounds.
+            is_bound[statement.entity.0 as usize] = true;
+
+            if let QueryValue::Var(v) = statement.value {
+                is_bound[v.0 as usize] = true;
+            }
+        }
+
+        // From the bound/free bitmap, produce two collections with variables.
+        // These are mainly used for convenience.
+        let mut bound_variables = Vec::with_capacity(variable_names.len());
+        let mut free_variables = Vec::with_capacity(variable_names.len());
+        let mut was_bound = true;
+        for (i, &bound) in is_bound.iter().enumerate() {
+            if bound {
+                bound_variables.push(Var(i as u16));
+                assert!(was_bound, "Bound variables must precede free variables.");
+            } else {
+                free_variables.push(Var(i as u16));
+                was_bound = false;
+            }
+        }
+
+        let query_mut = QueryMut {
+            variable_names: variable_names,
+            where_statements: where_statements,
+            assertions: assertions,
+            bound_variables: bound_variables,
+            free_variables: free_variables,
+            select: selects,
+        };
+
+        Ok(query_mut)
+    }
+
+    /// Return the read-only part of the query.
+    ///
+    /// For every tuple in the result of this read-only query, the assertions
+    /// will produce new datoms.
+    pub fn read_only_part(&self) -> Query {
+        Query {
+            // TODO: Avoid the clones.
+            variable_names: self.variable_names.clone(),
+            where_statements: self.where_statements.clone(),
+            select: self.bound_variables.clone(),
+        }
     }
 }
