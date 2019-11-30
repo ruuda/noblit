1036b308858d0aed60906c907365919671e70fd4
Ruud van Asseldonk <dev@veniogames.com>
2019-11-30 19:31:53 +0100

    Upgrade to latest commit in nixos-unstable channel
      curl                        7.66.0 -> 7.67.0
      python3.7-psutil            5.6.3  -> 5.6.5
      python3.7-typing_extensions 3.7.4  -> 3.7.4.1

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/e89b21504f3e61e535229afa0b121defb52d2a50.tar.gz";
-  sha256 = "0jqcv3rfki3mwda00g66d27k6q2y7ca5mslrnshfpbdm7j8ya0kj";
+  url = "https://github.com/NixOS/nixpkgs/archive/3ccbc8d89153ecf13f3eae7d9c106d91cd4ab9e5.tar.gz";
+  sha256 = "1mgqx1h8i9npxzmywdhkgm67iwsa0rvicwjs5wjssxlzx0hf6z5z";
 }
