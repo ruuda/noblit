500f8726511a9e4a7f6b5fa81707ad89ff18abbb
Ruud van Asseldonk <dev@veniogames.com>
2020-02-02 20:54:33 +0100

    Add libgmp to Nix dependences
    Now that I am taking binutils from Nix, suddenly the "stack setup" step
    fails in ld.gold from binutils from the Nix store. Perhaps adding the
    binutils package caused a new linker to be on the path, and that linker
    does not search outside of he Nix store? In any case, libgmp *is* a
    dependency of the Haskell runtime, so it being absent from the devenv
    was a real bug.

--- default.nix
+++ default.nix
@@ -6,6 +6,7 @@ in
     paths = [
       pkgs.binutils # Provides "nm", for the symbol sanity check.
       pkgs.gcc      # For checking the generated C header.
+      pkgs.gmp      # Required by the Haskell runtime.
       pkgs.mkdocs   # For building documentation.
       pkgs.mypy     # For typechecking the Python code.
       pkgs.perl     # The "prove" test harness is part of Perl.
