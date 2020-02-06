ac0c992e4a266456e55efa5d811c44ad60c771d3
Ruud van Asseldonk <dev@veniogames.com>
2020-02-06 23:47:46 +0100

    Run Nix only once on CI to set up environment
    The initial call to nix-build on CI takes about 40 seconds. The next one
    takes 1 second (just for lib path, should be a no-op), and the one after
    that takes 5 seconds. I expect we can save almost 6 seconds by doing
    everything in a single nix-build call, and saving the path. It also
    allows us to get rid of one file.

--- default.nix
+++ default.nix
@@ -4,14 +4,15 @@ in
   pkgs.buildEnv {
     name = "noblit-devenv";
     paths = [
-      pkgs.binutils # Provides "nm", for the symbol sanity check.
-      pkgs.gcc      # For checking the generated C header.
-      pkgs.gmp      # Required by the Haskell runtime.
-      pkgs.mkdocs   # For building documentation.
-      pkgs.mypy     # For typechecking the Python code.
-      pkgs.perl     # The "prove" test harness is part of Perl.
-      pkgs.python3  # For running the golden tests and other tools.
-      pkgs.rustup   # Provides the Rust toolchain.
-      pkgs.stack    # Provides the Haskell toolchain.
+      pkgs.binutils     # Provides "nm", for the symbol sanity check.
+      pkgs.gcc          # For checking the generated C header.
+      pkgs.glibcLocales # Needed for the locale-archive, to avoid LC_* errors.
+      pkgs.gmp          # Required by the Haskell runtime.
+      pkgs.mkdocs       # For building documentation.
+      pkgs.mypy         # For typechecking the Python code.
+      pkgs.perl         # The "prove" test harness is part of Perl.
+      pkgs.python3      # For running the golden tests and other tools.
+      pkgs.rustup       # Provides the Rust toolchain.
+      pkgs.stack        # Provides the Haskell toolchain.
     ];
   }
