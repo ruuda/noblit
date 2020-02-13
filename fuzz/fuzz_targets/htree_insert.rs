15031f301710a4c0e33adcc8d3f5f087f7f6232f
Ruud van Asseldonk <dev@veniogames.com>
2020-02-13 22:31:40 +0100

    Add smarter structure-aware fuzzer for htree
    I already had two structure-aware fuzzers for the htree: htree_insert_-
    value, and htree_insert_entity. For the structure-aware part, I wrote
    a byte-array-to-struct decoder by hand. But the "arbitrary" crate
    recently got an overhaul, and it does basically the same thing, but it
    does two things better:
    
     * It uses length suffixes instead of length prefixes. Apparently that
       works better with libFuzzer's mutations, which makes sense to me,
       because if a mutation messes up a length prefix, then everything
       behind gets a new meaning, so putting the length at the end may help.
    
     * It has #[derive(Arbitrary)], which is really *really* nice to avoid
       boilerplate.
    
    This is not without downsides though: adding a dependency on the
    "arbitrary" crate, and its deriving functionality especially, brings in
    many new dependencies. Fortunately these are only required for fuzzing,
    and not needed for Noblit itself, not even for building it, so I think
    this is acceptable.
    
    The new unified "htree_insert" fuzzer supersedes both existing fuzzers;
    it now generates both unique entities and unique values in the same
    fuzzer. Also, the value fuzzer now generates both integers and byte
    strings, not only byte strings. This is mainly because Arbitrary makes
    it so easy to do this, and previously I just had not gotten around to
    writing it.
    
    In terms of coverage: after 30 minutes or so, the new fuzzer reaches a
    higher feature and coverage count than either previous fuzzer, but that
    is no surprise, because the new (derived) test case generation now has a
    lot more branches in it. So I am not sure what the true effect on
    coverage is. I assume both get to 100%.
    
    In terms of speed: runs per second is in the same ballpark as the old
    fuzzers for a release build with debug assertions enabled.

---
+++ fuzz/fuzz_targets/htree_insert.rs
@@ -0,0 +1,129 @@
+// Noblit -- An immutable append-only database
+// Copyright 2020 Ruud van Asseldonk
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License has been included in the root of the repository.
+
+//! Fuzz test that asserts tree invariants after insertions.
+
+#![no_main]
+
+extern crate arbitrary;
+extern crate libfuzzer_sys;
+extern crate noblit;
+
+use std::cmp::Ordering;
+
+use arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
+
+use noblit::datom::{Datom, Aid, Eid, Value, Tid};
+use noblit::heap::{HeapMut, self};
+use noblit::htree::{HTree, Node};
+use noblit::index::{DatomOrd, Eavt};
+use noblit::memory_store::{MemoryStore, MemoryHeap};
+use noblit::store::{StoreMut, PageSize, PageSize256, PageSize568, PageSize4096};
+
+#[derive(Arbitrary, Debug)]
+enum FuzzValue {
+    U64(u64),
+    Bytes(Vec<u8>),
+}
+
+#[derive(Arbitrary, Debug)]
+enum FuzzAction {
+    Append { entity: u64, attribute: u64, value: FuzzValue },
+    Insert,
+}
+
+#[derive(Arbitrary, Debug)]
+enum FuzzInput {
+    RunPageSize256 { actions: Vec<FuzzAction> },
+    RunPageSize568 { actions: Vec<FuzzAction> },
+    RunPageSize4096 { actions: Vec<FuzzAction> },
+}
+
+fn run<Size: PageSize>(actions: &[FuzzAction]) -> Option<()> {
+    let mut store = MemoryStore::<Size>::new();
+    let mut heap = MemoryHeap::new();
+
+    let node = Node::empty_of_level(0);
+    let root = store.write_page(&node.write::<Size>()).unwrap();
+    let mut tree = HTree::new(root, Eavt, store, &mut heap);
+
+    // Reserve capacity for a large-ish number of datoms, to prevent most
+    // reallocations during fuzzing, thereby reducing distracting control flow.
+    let mut datoms = Vec::with_capacity(255);
+    let mut tid = 0;
+
+    for action in actions {
+        match action {
+            FuzzAction::Append { entity, attribute, value: fuzz_value } => {
+                let value = match fuzz_value {
+                    FuzzValue::U64(x) => {
+                        match Value::from_u64(*x) {
+                            Some(v) => v,
+                            None => {
+                                let cid = tree.heap.append_u64(*x).unwrap();
+                                heap::check_invariants(&tree.heap);
+                                Value::from_const_u64(cid)
+                            }
+                        }
+                    }
+                    FuzzValue::Bytes(bytes) => {
+                        match Value::from_bytes(bytes) {
+                            Some(v) => v,
+                            None => {
+                                let cid = tree.heap.append_bytes(bytes).unwrap();
+                                heap::check_invariants(&tree.heap);
+                                Value::from_const_bytes(cid)
+                            }
+                        }
+                    }
+                };
+
+                let datom = Datom::assert(Eid(*entity), Aid(*attribute), value, Tid(tid));
+
+                // We perform an insertion sort of the datoms to insert the new
+                // datom, exiting on duplicates. This serves two purposes:
+                // * Tree insertion requires the datoms to insert to be sorted.
+                //   We can achieve this by sorting on the fly rather than
+                //   sorting afterwards. Because insertion sort is so simple,
+                //   there is less control flow that the fuzzer considers
+                //   interesting than for e.g. `slice.sort_by`, which puts more
+                //   focus the tree insertion that we are actually interested in.
+                // * Tree insertion also requires unique datoms. By exiting on
+                //   duplicates, rather than skipping the insertion, we force
+                //   the fuzzer to focus on interesting inputs, rather than
+                //   inserting useless zeros everywhere.
+                let mut insert_index = datoms.len();
+                for i in 0..datoms.len() {
+                    match (&tree.comparator as &dyn DatomOrd).cmp(&datom, &datoms[i], &tree.heap) {
+                        Ordering::Less => { insert_index = i; break }
+                        Ordering::Equal => return None,
+                        Ordering::Greater => continue,
+                    }
+                }
+                datoms.insert(insert_index, datom);
+            }
+            FuzzAction::Insert => {
+                tree.insert(&datoms[..]).unwrap();
+                tree.check_invariants().unwrap();
+                datoms.clear();
+                // Transaction ids must be even.
+                tid += 2;
+            }
+        }
+    }
+
+    Some(())
+}
+
+fuzz_target!(|input: FuzzInput| {
+    match input {
+        FuzzInput::RunPageSize256 { actions } => run::<PageSize256>(&actions),
+        FuzzInput::RunPageSize568 { actions } => run::<PageSize568>(&actions),
+        FuzzInput::RunPageSize4096 { actions } => run::<PageSize4096>(&actions),
+    };
+});
