3d43c9a0c06da0ae07403bd28fff7cf3718cfed5
Ruud van Asseldonk <dev@veniogames.com>
2020-02-15 22:10:28 +0100

    Expose errors from the Python client
    This wraps error results in a NoblitError exception, which contains the
    result code, and the message.

--- client/python/noblit.py
+++ client/python/noblit.py
@@ -6,7 +6,9 @@
 # A copy of the License has been included in the root of the repository.
 from __future__ import annotations
 
-from ctypes import CDLL, byref, cdll, c_char_p, c_uint32, c_size_t, c_void_p, c_int
+from ctypes import CDLL, POINTER, Structure, byref, cdll
+from ctypes import c_byte, c_char, c_char_p, c_uint32, c_size_t, c_void_p, c_int
+from enum import Enum
 from typing import IO, Optional, NamedTuple
 
 _LIB: Optional[CDLL] = None
@@ -41,6 +43,8 @@ def ensure_lib() -> CDLL:
     if _LIB is None:
         _LIB = load_lib()
 
+        _LIB.noblit_get_last_error.argtypes = [c_void_p]
+        _LIB.noblit_get_last_error.restype = _Slice
         _LIB.noblit_open_packed_in_memory.argtypes = [c_int]
         _LIB.noblit_open_packed_in_memory.restype = c_void_p
         _LIB.noblit_close.argtypes = [c_void_p]
@@ -51,29 +55,70 @@ def ensure_lib() -> CDLL:
     return _LIB
 
 
+class _Slice(Structure):
+    _fields_ = [('data', POINTER(c_byte)), ('len', c_size_t)]
+
+    def into_bytes(self) -> bytes:
+        return POINTER(c_char).from_buffer(self.data)[:self.len]
+
+
+class NoblitResult(Enum):
+    """
+    Result codes that can be returned from the Noblit API.
+    """
+    OK = 0
+    IO_ERROR = 1
+
+
+class NoblitError(Exception):
+    """
+    Raised when a Noblit FFI call returned a non-ok result.
+
+    Contains the result code, which is the error category, and the message with
+    more details, including details of this particular instance of the error.
+    """
+    def __init__(self, result: int, message: str) -> None:
+        self.result = NoblitResult(result)
+        self.message = message
+
+    def __str__(self) -> str:
+        return self.message
+
+
 class Database:
     def __init__(self, db: c_void_p) -> None:
         self._db = db
         self._lib = ensure_lib()
 
+    def _get_last_error(self) -> str:
+        message_utf8 = self._lib.noblit_get_last_error(self._db).into_bytes()
+        return message_utf8.decode('utf-8')
+
+    def _check_result(self, result: c_uint32) -> None:
+        if result == 0:
+            return
+
+        message = self._get_last_error()
+        raise NoblitError(result, message)
+
+
     @staticmethod
     def open_packed_in_memory(dbfile: IO[bytes]) -> Database:
         lib = ensure_lib()
         db = lib.noblit_open_packed_in_memory(dbfile.fileno())
-        return Database(db)
+        return Database(c_void_p(db))
 
     def __del__(self) -> None:
         self._lib.noblit_close(self._db)
 
-    def _inspect_result(self, result: c_uint32) -> None:
-        if result != 0:
-            # TODO: Get the error message.
-            raise Exception('Someting went wrong.')
+    def __repr__(self) -> str:
+        return f'Database(0x{self._db.value:x})'
 
     def query(self, q: bytes) -> Evaluator:
         evaluator = c_void_p()
         result = self._lib.noblit_query_open(self._db, q, len(q), byref(evaluator))
-        self._inspect_result(result)
+        self._check_result(result)
+        assert evaluator.value is not None
         return Evaluator(self._lib, evaluator)
 
 
@@ -85,9 +130,19 @@ class Evaluator:
     def __del__(self) -> None:
         self._lib.noblit_query_close(self._evaluator)
 
+    def __repr__(self) -> str:
+        return f'Evaluator(0x{self._evaluator.value:x})'
+
 
 with open('../mindec/mindec.ndb', 'rb') as f:
     db = Database.open_packed_in_memory(f)
     print(db)
-    q = db.query(b'')
-    print(q)
+    query_builtin_types = (
+        b'\x00\x02\x00\x04\x00name\x01\x00t\x01\x00\x01\x00\x0c\x00'
+        b'db.type.name\x00\x00\x00\x02\x00\x01\x00\x00\x00'
+    )
+    evaluator = db.query(query_builtin_types)
+    print(evaluator)
+    del evaluator
+    print('Dropped evaluator')
+    del db
