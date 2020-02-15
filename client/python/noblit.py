de8b8558fd2f0b8dd622ff36171953c94f219823
Ruud van Asseldonk <dev@veniogames.com>
2020-02-15 11:34:31 +0100

    Define Python wrapper for query evaluator

--- client/python/noblit.py
+++ client/python/noblit.py
@@ -6,7 +6,7 @@
 # A copy of the License has been included in the root of the repository.
 from __future__ import annotations
 
-from ctypes import CDLL, cdll, c_char_p, c_size_t, c_void_p, c_int
+from ctypes import CDLL, byref, cdll, c_char_p, c_uint32, c_size_t, c_void_p, c_int
 from typing import IO, Optional, NamedTuple
 
 _LIB: Optional[CDLL] = None
@@ -45,6 +45,8 @@ def ensure_lib() -> CDLL:
         _LIB.noblit_open_packed_in_memory.restype = c_void_p
         _LIB.noblit_close.argtypes = [c_void_p]
         _LIB.noblit_close.restype = None
+        _LIB.noblit_query_open.argtypes = [c_void_p, c_char_p, c_size_t, c_void_p]
+        _LIB.noblit_query_open.retype = c_uint32
 
     return _LIB
 
@@ -52,6 +54,7 @@ def ensure_lib() -> CDLL:
 class Database:
     def __init__(self, db: c_void_p) -> None:
         self._db = db
+        self._lib = ensure_lib()
 
     @staticmethod
     def open_packed_in_memory(dbfile: IO[bytes]) -> Database:
@@ -60,10 +63,31 @@ class Database:
         return Database(db)
 
     def __del__(self) -> None:
-        lib = ensure_lib()
-        lib.noblit_close(self._db)
+        self._lib.noblit_close(self._db)
+
+    def _inspect_result(self, result: c_uint32) -> None:
+        if result != 0:
+            # TODO: Get the error message.
+            raise Exception('Someting went wrong.')
+
+    def query(self, q: bytes) -> Evaluator:
+        evaluator = c_void_p()
+        result = self._lib.noblit_query_open(self._db, q, len(q), byref(evaluator))
+        self._inspect_result(result)
+        return Evaluator(self._lib, evaluator)
+
+
+class Evaluator:
+    def __init__(self, lib: CDLL, evaluator: c_void_p) -> None:
+        self._lib = lib
+        self._evaluator = evaluator
+
+    def __del__(self) -> None:
+        self._lib.noblit_query_close(self._evaluator)
 
 
 with open('../mindec/mindec.ndb', 'rb') as f:
     db = Database.open_packed_in_memory(f)
     print(db)
+    q = db.query(b'')
+    print(q)
