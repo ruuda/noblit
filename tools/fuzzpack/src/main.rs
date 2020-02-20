30d13ab57bd07e3fc9f7999b72d5b98aaf2e2c11
Ruud van Asseldonk <dev@veniogames.com>
2020-02-20 21:17:31 +0100

    Implement fst-based fuzz corpus packer
    Spoiler alert: it is not smaller than the sorted Python packer after
    compression, and only barely smaller without compression:
    
    Version  Compression     Bytes
    -------  -----------  --------
    fst      none         66600202
    fst      brotli -9      966903
    python   none         75526719
    python   brotli -9      615054
    
    So far for this experiment, then.

--- tools/fuzzpack/src/main.rs
+++ tools/fuzzpack/src/main.rs
@@ -3,6 +3,7 @@ extern crate fst;
 
 use std::env;
 use std::fs;
+use std::io;
 
 fn load_corpus(corpus_path: &str) -> Vec<Vec<u8>> {
     let mut fnames = Vec::new();
@@ -15,15 +16,27 @@ fn load_corpus(corpus_path: &str) -> Vec<Vec<u8>> {
 
     let mut corpus = Vec::new();
     for (i, fname) in fnames.iter().enumerate() {
-        eprint!("\r[{} / {}] {:?}", i + 1, fnames.len(), fname);
+        if i % 32 == 0 {
+            eprint!("\r[{} / {}] {:?}", i + 1, fnames.len(), fname);
+        }
         let entry = fs::read(fname).expect("Failed to read entry.");
         corpus.push(entry);
     }
+    eprintln!();
+
     corpus 
 }
 
 fn main() {
     let corpus_path = env::args().nth(1).expect("Expected corpus path arg.");
     let mut corpus = load_corpus(&corpus_path);
-    println!("Have {} files.", corpus.len());
+    println!("{} files in corpus.", corpus.len());
+    corpus.sort();
+
+    let stdout = io::stdout();
+    let mut builder = fst::SetBuilder::new(stdout.lock()).expect("Failed to open writer.");
+    for entry in &corpus {
+        builder.insert(entry).expect("Failed to insert entry.");
+    }
+    builder.finish().expect("Failed to finalize builder.");
 }
