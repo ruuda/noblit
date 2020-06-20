ad3f519791dfa86d931472d275988e04ec215735
Ruud van Asseldonk <dev@veniogames.com>
2020-06-20 18:48:35 +0200

    Move Nix import into nixpkgs-pinned.nix
    This brings it in line with the convention in my other repositories,
    which means I can use my update script to update the pinned Nixpkgs
    snapshot, and get a commit message with the changed versions.

--- nixpkgs-pinned.nix
+++ nixpkgs-pinned.nix
@@ -1,4 +1,4 @@
-fetchTarball {
+import (fetchTarball {
   url = "https://github.com/NixOS/nixpkgs/archive/e1eedf29e5d22e6824e614d75449b75a2e3455d6.tar.gz";
   sha256 = "1v237cgfkd8sb5f1r08sms1rxygjav8a1i1jjjxyqgiszzpiwdx7";
-}
+})
